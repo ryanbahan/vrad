94901aaf2fc5e3bfad56e2344b4d41b7527352bb
Tim Nguyen <tim.nguyen2112@gmail.com>
2020-03-24 13:27:59 -0600

    Add intial fetch of Areas data

--- src/components/AreasContainer/AreasContainer.js
+++ src/components/AreasContainer/AreasContainer.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import './AreasContainer.scss';
 
-function AreasContainer(props) {
-  return <main className="areas-container"></main>
-}
+
+
+
+class AreasContainer extends React.Component {
+  fetchAreaData = () => {
+    fetch('http://localhost:3001/api/v1/areas')
+     .then(res => res.json())
+     .then(data => {
+      const promises = data.areas.map(area => {
+        return fetch(`http://localhost:3001${area.details}`)
+              .then(res => console.log(res.json()))
+      })
+    })
+  }
+
+  componentDidMount() {
+    this.fetchAreaData()
+  }
+
+  render() {
+    return(
+      <main className="areas-container">hhh</main>
+
+      )
+    }
+  }
 
 export default AreasContainer;
