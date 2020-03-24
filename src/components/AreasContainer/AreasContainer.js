6436c18e5fcfa9e44f62a3ec9d843ff92537f258
Tim Nguyen <tim.nguyen2112@gmail.com>
2020-03-24 13:38:19 -0600

    Add fetch functionality

--- src/components/AreasContainer/AreasContainer.js
+++ src/components/AreasContainer/AreasContainer.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import './AreasContainer.scss';
 
-
-
-
 class AreasContainer extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      areas: []
+    }
+  }
+
+
   fetchAreaData = () => {
+    // console.log("qwq")
     fetch('http://localhost:3001/api/v1/areas')
      .then(res => res.json())
      .then(data => {
       const promises = data.areas.map(area => {
         return fetch(`http://localhost:3001${area.details}`)
-              .then(res => console.log(res.json()))
+              .then(res => res.json())
+              this.setState({areas: Promise.all(promises)})
+              console.log(Promise.all(promises))
       })
     })
   }
