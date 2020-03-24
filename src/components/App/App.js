477f535373e7d19b4a4f5c7b72b05597c0c30e33
Ryan Bahan <ryanbahan@Ryans-iMac.local>
2020-03-24 11:30:46 -0600

    Add initial form

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -17,7 +17,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-      <p>Test</p>
+      <Login />
       </div>
     );
   }
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -11,8 +11,20 @@ class Login extends React.Component {
 
   render() {
     return (
-      <main className="Login">
-      <p>Login test</p>
+      <main className="login-container">
+        <form className="login-form">
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" />
+        <label htmlFor="email">Email Address</label>
+        <input type="email" id="email" />
+        <label htmlFor="account-type">Account Type</label>
+        <select name="account-type" id="account-type">
+        <option value="cat">Vacation</option>
+          <option value="dog">Business</option>
+          <option value="hamster">Other</option>
+        </select>
+        <button>Sign Up</button>
+        </form>
       </main>
     );
   }
