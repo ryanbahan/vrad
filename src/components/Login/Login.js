78918e67e857b7fa6d99ac13334f2858ee65b98c
Ryan Bahan <ryanbahan@Ryans-iMac.local>
2020-03-24 11:38:56 -0600

    Create methods for controlled form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,23 +5,41 @@ class Login extends React.Component {
   constructor() {
     super();
     this.state = {
-
+      name: null,
+      email: null,
+      accountType: "vacation"
     }
+  }
+
+  updateName = (e) => {
+    this.setState({name: e.target.value});
+  }
+
+  updateEmail = (e) => {
+    this.setState({email: e.target.value});
+  }
+
+  updateAccountType = (e) => {
+    this.setState({accountType: e.target.value});
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
   }
 
   render() {
     return (
       <main className="login-container">
-        <form className="login-form">
+        <form className="login-form" onSubmit={this.handleSubmit}>
         <label htmlFor="name">Name</label>
-        <input type="text" id="name" />
+        <input type="text" id="name" onChange={this.updateName} required/>
         <label htmlFor="email">Email Address</label>
-        <input type="email" id="email" />
+        <input type="email" id="email" onChange={this.updateEmail} required/>
         <label htmlFor="account-type">Account Type</label>
-        <select name="account-type" id="account-type">
-        <option value="cat">Vacation</option>
-          <option value="dog">Business</option>
-          <option value="hamster">Other</option>
+        <select name="account-type" id="account-type" onChange={this.updateAccountType} required>
+        <option value="vacation">Vacation</option>
+          <option value="business">Business</option>
+          <option value="other">Other</option>
         </select>
         <button>Sign Up</button>
         </form>
