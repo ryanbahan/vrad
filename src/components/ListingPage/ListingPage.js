0cc35c1b829ed768e53a4346a189f4e40e8dc7b9
Ryan Bahan <ryanbahan@Ryans-iMac.local>
2020-03-25 17:13:42 -0600

    Add listing page functionality

--- src/components/ListingPage/ListingPage.js
+++ src/components/ListingPage/ListingPage.js
@@ -5,7 +5,14 @@ class ListingPage extends React.Component {
   constructor({ match, location }) {
     super();
     this.state = {
-      listing: null,
+      listing: {
+        address: {
+          street: ""
+        },
+        details: {
+          features: []
+        }
+      },
       id: match.params.id
     }
   }
@@ -20,8 +27,35 @@ class ListingPage extends React.Component {
     this.fetchListingPageData();
   }
 
+  getFeatures = () => {
+    const features = this.state.listing.details.features;
+    return features.map(feature => {
+      return <li key={feature}>{feature}</li>
+    })
+  }
+
   render() {
-    return <main className="listing-page">test</main>
+    const { name, area, address } = this.state.listing;
+    const { baths, beds, cost_per_night, superhost } = this.state.listing.details;
+
+    return <main className="listing-page">
+      <div className="top-container">
+        <h2 className="listing-title">{name}</h2>
+        <p className="address">{address.street}</p>
+      </div>
+      <img className="main-image" src={"/images/" + this.state.id + "_a.jpg"} />
+      <div className="secondary-images">
+        <img className="secondary-image-a" src={"/images/" + this.state.id + "_b.jpg"} />
+        <img className="secondary-image-b" src={"/images/" + this.state.id + "_c.jpg"} />
+      </div>
+      <p className="listing-area-shortname">{area}</p>
+      <p className="image-lower-bar">{"$" + cost_per_night + " per night / " + beds + " beds / " + baths + " baths"}</p>
+      <h3 className="features-title">Features</h3>
+      <ul className="features-list">
+        {this.getFeatures()}
+      </ul>
+      <button type="button" className="favorite-button">Favorite</button>
+    </main>
   }
 }
 
