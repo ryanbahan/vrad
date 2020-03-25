49d4476833da83318a95370ff0d29650dff8d2c7
Tim Nguyen <tim.nguyen2112@gmail.com>
2020-03-24 20:55:46 -0600

    Add css to the listing card ListingsContainer

--- src/components/ListingCard/ListingCard.js
+++ src/components/ListingCard/ListingCard.js
@@ -5,11 +5,13 @@ function ListingCard(props) {
   return <article className="listing-card">
     <section>
       <h1>{props.name}</h1>
-      <button>View Listing</button>
-      <button>Add to Favorite</button>
+      <section>
+        <button>View Listing</button>
+        <button>Add to Favorite</button>
+      </section>
     </section>
     <section>
-      <img src= {`/images/${props.id}_a.jpg`} />
+      <img className="listing-img" src= {`/images/${props.id}_a.jpg`} />
     </section>
   </article>
 }
