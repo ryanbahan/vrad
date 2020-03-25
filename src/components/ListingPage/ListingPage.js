5b27bfe0b58e59d2a4ac3a960ee141a88d6413b7
Ryan Bahan <ryanbahan@Ryans-iMac.local>
2020-03-25 15:39:22 -0600

    Add fetch for listing item

--- src/components/ListingPage/ListingPage.js
+++ src/components/ListingPage/ListingPage.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import './ListingPage.scss';
 
-function ListingPage(props) {
-  return <main className="listing-page">test</main>
+class ListingPage extends React.Component {
+  constructor({ match, location }) {
+    super({ match, location });
+    this.state = {
+      id: match.params.id
+    }
+  }
+
+  fetchListingPageData = () => {
+    fetch("http://localhost:3001/api/v1/listings/" + this.state.id)
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }
+
+  componentDidMount() {
+    this.fetchListingPageData();
+  }
+
+  render() {
+    console.log(this.props);
+    return <main className="listing-page">test</main>
+  }
 }
 
 export default ListingPage;
--- src/components/ListingsContainer/ListingsContainer.js
+++ src/components/ListingsContainer/ListingsContainer.js
@@ -5,7 +5,7 @@ import './ListingsContainer.scss';
 
 class ListingsContainer extends React.Component {
   constructor({ match, location }) {
-    super();
+    super({ match, location });
     this.state = {
       listings: []
     }
