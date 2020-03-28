71fa34759ddbf3443a4a599c6d39f17c2bb0824f
Tim Nguyen <tim.nguyen2112@gmail.com>
2020-03-28 17:09:45 -0600

    Add fetch data of listing ID in local storage

--- src/components/FavoriteContainer/FavoriteContainer.js
+++ src/components/FavoriteContainer/FavoriteContainer.js
@@ -6,14 +6,36 @@ class FavoriteContainer extends React.Component {
   constructor() {
     super();
     this.state = {
-      favorite: []
+      listings: []
     }
   }
+
+  componentDidMount() {
+    this.fetchListingData();
+  }
+
+  fetchListingData = () => {
+    const favorites = JSON.parse(window.localStorage.getItem("listingFavorites"));
+    const promises = favorites.map(listingID => {
+      return fetch('http://localhost:3001/api/v1/listings/' + listingID)
+      .then(res => res.json())
+      .then(data => {
+        return {id: listingID,
+          areaID: data.area_id,
+          name: data.name}
+      })
+    })
+    Promise.all(promises).then(listings => this.setState({listings}));
+  }
+
 
   render() {
     return<main>
     <Nav user={this.props.user} />
-    <h1>Display Favorite Card Listing</h1>
+    <h1>Favorite Listing</h1>
+      <section>
+        lll
+      </section>
     </main>
   }
 }
