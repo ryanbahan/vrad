cb6f49b2a5f64142a1a6b5140244af8a36d6f1c3
Ryan Bahan <ryanbahan@Ryans-iMac.local>
2020-03-29 13:49:58 -0600

    Add test for listings

--- src/components/ListingsContainer/ListingsContainer.test.js
+++ src/components/ListingsContainer/ListingsContainer.test.js
@@ -15,24 +15,41 @@ describe("ListingsContainer", () => {
         <ListingsContainer
         user={"Ryan B"}
         match={
-          {params: {id: 5}}
+          {params: {id: 590}}
         }
         />
       </Router>)
   });
 
   it("should display the user's name with a greeting", () => {
-    const { debug, getByText } = utils;
+    const { getByText } = utils;
     const name = getByText('Welcome, Ryan B');
 
     expect(name).toBeInTheDocument();
   })
 
   it("Should display the page label", () => {
-    const { debug, getByText } = utils;
+    const { getByText } = utils;
     const label = getByText("Choose Your Listing");
 
     expect(label).toBeInTheDocument();
+  })
+
+  it("Should be able to get listings", async () => {
+    const { queryByText } = utils;
+    const listingA = await waitForElement(() => queryByText('Hip RiNo Party Spot'));
+    const listingB = await waitForElement(() => queryByText('Lowkey Industrial Chic'));
+    const listingC = await waitForElement(() => queryByText('New Modern Flat in RiNo'));
+    const listingD = await waitForElement(() => queryByText('Upscale Modern Apartments'));
+    const listingE = await waitForElement(() => queryByText('Loft Living in RiNo'));
+    const listingF = await waitForElement(() => queryByText('Brand New RiNo Build'));
+
+    expect(listingA).toBeInTheDocument();
+    expect(listingB).toBeInTheDocument();
+    expect(listingC).toBeInTheDocument();
+    expect(listingD).toBeInTheDocument();
+    expect(listingE).toBeInTheDocument();
+    expect(listingF).toBeInTheDocument();
   })
 
 })
