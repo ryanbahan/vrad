1013268de1c54284787b46ba34b9963746de4193
Ryan Bahan <ryanbahan@Ryans-iMac.local>
2020-03-29 14:09:05 -0600

    Add tests for all area views

--- src/components/ListingsContainer/ListingsContainer.test.js
+++ src/components/ListingsContainer/ListingsContainer.test.js
@@ -68,10 +68,120 @@ describe("ListingsContainer - Park Hill View", () => {
       </Router>)
   })
 
-  it("should display the user's name with a greeting", () => {
+  it("Should display the user's name with a greeting", () => {
     const { getByText } = utils;
     const name = getByText('Welcome, Tim N');
 
     expect(name).toBeInTheDocument();
+  })
+
+  it("Should display the page label", () => {
+    const { getByText } = utils;
+    const label = getByText("Choose Your Listing");
+
+    expect(label).toBeInTheDocument();
+  })
+
+  it("Should be able to get listings", async () => {
+    const { queryByText } = utils;
+    const listingA = await waitForElement(() => queryByText('Spacious New Build in Park Hill'));
+    const listingB = await waitForElement(() => queryByText('Updated Park Hill Duplex'));
+    const listingC = await waitForElement(() => queryByText('Historic Park Hill abode'));
+
+    expect(listingA).toBeInTheDocument();
+    expect(listingB).toBeInTheDocument();
+    expect(listingC).toBeInTheDocument();
+  })
+})
+
+describe("ListingsContainer - LoHi View", () => {
+  let utils;
+
+  beforeEach(() => {
+    utils = render(
+      <Router>
+        <ListingsContainer
+        user={"Khalid W"}
+        match={
+          {params: {id: 408}}
+        }
+        />
+      </Router>)
+  })
+
+  it("Should display the user's name with a greeting", () => {
+    const { getByText } = utils;
+    const name = getByText('Welcome, Khalid W');
+
+    expect(name).toBeInTheDocument();
+  })
+
+  it("Should display the page label", () => {
+    const { getByText } = utils;
+    const label = getByText("Choose Your Listing");
+
+    expect(label).toBeInTheDocument();
+  })
+
+  it("Should be able to get listings", async () => {
+    const { queryByText } = utils;
+    const listingA = await waitForElement(() => queryByText('LoHi Heaven'));
+    const listingB = await waitForElement(() => queryByText('LoHi Bungalow'));
+    const listingC = await waitForElement(() => queryByText('Quaint Lohi Duplex'));
+    const listingD = await waitForElement(() => queryByText('Downtown Views Galore'));
+    const listingE = await waitForElement(() => queryByText('LoHi Super Pad'));
+    const listingF = await waitForElement(() => queryByText('Gorgeous LoHi Home'));
+    const listingG = await waitForElement(() => queryByText('Open Layout LoHi Getaway'));
+
+    expect(listingA).toBeInTheDocument();
+    expect(listingB).toBeInTheDocument();
+    expect(listingC).toBeInTheDocument();
+    expect(listingD).toBeInTheDocument();
+    expect(listingE).toBeInTheDocument();
+    expect(listingF).toBeInTheDocument();
+    expect(listingG).toBeInTheDocument();
+  })
+})
+
+describe("ListingsContainer - Cap Hill View", () => {
+  let utils;
+
+  beforeEach(() => {
+    utils = render(
+      <Router>
+        <ListingsContainer
+        user={"Robbie J"}
+        match={
+          {params: {id: 240}}
+        }
+        />
+      </Router>)
+  })
+
+  it("Should display the user's name with a greeting", () => {
+    const { getByText } = utils;
+    const name = getByText('Welcome, Robbie J');
+
+    expect(name).toBeInTheDocument();
+  })
+
+  it("Should display the page label", () => {
+    const { getByText } = utils;
+    const label = getByText("Choose Your Listing");
+
+    expect(label).toBeInTheDocument();
+  })
+
+  it("Should be able to get listings", async () => {
+    const { queryByText } = utils;
+    const listingA = await waitForElement(() => queryByText('Cap Hill Overlook'));
+    const listingB = await waitForElement(() => queryByText('Cap Hill Duplex Delight'));
+    const listingC = await waitForElement(() => queryByText('Cap Hill Castle'));
+    const listingD = await waitForElement(() => queryByText('Elegant Cap Hill Mansion'));
+
+    expect(listingA).toBeInTheDocument();
+    expect(listingB).toBeInTheDocument();
+    expect(listingC).toBeInTheDocument();
+    expect(listingD).toBeInTheDocument();
   })
 })
