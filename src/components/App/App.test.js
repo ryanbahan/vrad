91831b4d05101a23177c96ea7cc35493b7fc0d0a
Tim Nguyen <tim.nguyen2112@gmail.com>
2020-03-30 02:32:25 -0600

    Add testing import to component test

---
+++ src/components/App/App.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render, waitForElement, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom/';
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import App from './App';
+
+describe("App", () => {
+  it("should..", () => {
+
+  })
+
+})
