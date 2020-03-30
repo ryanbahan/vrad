e20134806d4eecbb19d378cb5940c75915260cff
Tim Nguyen <tim.nguyen2112@gmail.com>
2020-03-30 02:33:52 -0600

    Add temporary skips on broken tests

--- src/components/Login/Login.test.js
+++ src/components/Login/Login.test.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect'
+import '@testing-library/jest-dom/';
 import { BrowserRouter as Router, Link } from "react-router-dom";
 import Login from './Login';
 
 describe("Login", () => {
   it("should store Login information", () => {
-
-    // const { getByPlaceholderText, getByText } = render(<Router><Login/></Router>)
+    // 
+    // const { getByPlaceholderText, getByText } = render(<Router><Login handleSubmit={jest.fn()}/></Router>)
     //
     // fireEvent.change(getByPlaceholderText('input-name'), {target: {value: "Tom"}});
     // fireEvent.change(getByPlaceholderText('input-email'), {target: {value: "email@email.com"}});
