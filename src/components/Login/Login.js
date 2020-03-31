53e6c1b1fab58e590423c25c3f4b842b4cac5bf7
Tim Nguyen <tim.nguyen2112@gmail.com>
2020-03-30 19:21:01 -0600

    Add additional testing for Login.test

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -41,11 +41,11 @@ class Login extends React.Component {
         <input type="email" id="email" placeholder="input-email" onChange={this.updateEmail} required/>
         <label htmlFor="account-type">Account Type</label>
         <select name="account-type" id="account-type" placeholder="account-type" onChange={this.updateAccountType} required>
-        <option value="vacation">Vacation</option>
+          <option value="vacation">Vacation</option>
           <option value="business">Business</option>
           <option value="other">Other</option>
         </select>
-          <button type="submit">Sign Up</button>
+          <button type="submit">Sign-Up</button>
         </form>
       </main>
     );
--- src/components/Login/Login.test.js
+++ src/components/Login/Login.test.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect'
-import '@testing-library/jest-dom/';
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import { BrowserRouter as Router} from "react-router-dom";
 import Login from './Login';
 
 describe("Login", () => {
